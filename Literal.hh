2819b2c633e534e43c832177f0e936ccb8408566
JulienLM <julienlm38@gmail.com>
2013-02-18 11:46:11 +0100

    Ajout de la classe Literal

---
+++ Literal.hh
@@ -0,0 +1,30 @@
+#ifndef LITERAL_HH
+#define LITERAL_HH
+
+
+class Literal {
+  protected:
+    unsigned int _var;
+    bool _pos;
+    
+  public:
+    inline Literal(unsigned int var, bool pos) : _var(var), _pos(pos) { };
+    inline Literal(const Literal& lit) : _var(lit._var), _pos(lit._pos) { };
+    
+    
+    inline unsigned int var(void) const {
+      return _var;
+    };
+    inline bool pos(void) const {
+      return _pos;
+    };
+    
+    inline bool operator <(const Literal& lit) const {
+      return _var < lit._var || (_var == lit._var && !_pos && lit._pos);
+    }
+};
+
+
+
+
+#endif//LITERAL_HH
