666e9f2b692730bb418fcd57cf7716126f336656
JulienLM <julienlm38@gmail.com>
2013-02-20 11:37:19 +0100

    ajout de sat_solver.hh (contient la classe SatPrblem qui résoud le CNF donné en entrée)

---
+++ sat_solver.hh
@@ -0,0 +1,34 @@
+#ifndef SAT_SOLVER_HH
+#define SAT_SOLVER_HH
+
+#include <vector>
+#include <set>
+#include <stack>
+
+typedef
+enum etat_var {
+  TRUE, FALSE, FREE
+} etat_var;
+
+
+class SatProblem {
+  protected:
+    vector<etat_var> _etats_var;
+    vector<pair<set<Clause*>,set<Clause*>> _variables;
+    stack<pair<bool,Literal>> _stack_callback;
+    
+  public:
+    SatProblem(unsigned int nbr_var, unsigned int nbr_clauses, const istream& input);
+    ~SatProblem();
+    
+    inline const vector<etat_var>& get_assign() const { return _etats_var; }
+    bool satisfiability();
+};
+    
+    
+    
+
+
+
+
+#endif//SAT_SOLVER_HH
