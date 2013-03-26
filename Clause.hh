4c20eb0a77f216a957f056b8b116337860a8fbbb
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2013-03-26 21:42:07 +0100

    MAJ Clause.hh comment

--- Clause.hh
+++ Clause.hh
@@ -5,23 +5,22 @@
 
 
 #if VERBOSE > 1
-#define CONSTR_ARGS(list)  std::vector<Literal>& list, const unsigned int number
-#define INIT_FOR_VERBOSE() _number(number),
-#define CALL_CONSTR(list)  list, number
-#include <iostream>
+    #define CONSTR_ARGS(list)  std::vector<Literal>& list, const unsigned int number
+    #define INIT_FOR_VERBOSE() _number(number),
+    #define CALL_CONSTR(list)  list, number
+    #include <iostream>
 #else
-#define CONSTR_ARGS(list)  std::vector<Literal>& list
-#define INIT_FOR_VERBOSE() 
-#define CALL_CONSTR(list)   list
+    #define CONSTR_ARGS(list)  std::vector<Literal>& list
+    #define INIT_FOR_VERBOSE() 
+    #define CALL_CONSTR(list)   list
 #endif
 
 
 #include "BasicClause.hh"
 #include "ConstAssignClause.hh"
-#include "OneWatchedClause.hh"
 #include "WatchedClause.hh"
 #include "SmartClause.hh"
 #include "SmartWatchedClause.hh"
 
 
-#endif //CLAUSE_HH
+#endif // CLAUSE_HH defined
