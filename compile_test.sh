e9011d83c9296cc1c749110e9730d90d98bea80a
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2013-04-09 19:53:57 +0200

    Un peu de nettoyage dans le dossiers

---
+++ compile_test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+make clean ;
+for clause in "Smart" "Watched"
+do (
+    for heur in "BASIC" "RAND" "DLIS"
+    do (
+        # avec tri
+        newName=exe${clause}${heur}AvecTri;
+        echo "***** Génération de $newName *****" ;
+        make CLAUSE=${clause}Clause CHOOSE=$heur VERBOSE=1 INIT_SORT=1 ;
+        mv release $newName ;
+        cp $newName testsSatisfiable/ ;
+        cp $newName testsUnsatisfiable/ ;
+        rm $newName ;
+        make clean ;
+        # sans tri
+        newName=exe${clause}${heur};
+        echo "***** Génération de $newName *****" ;
+        make CLAUSE=${clause}Clause CHOOSE=$heur VERBOSE=1 INIT_SORT=0 ;
+        mv release $newName ;
+        cp $newName testsSatisfiable/ ;
+        cp $newName testsUnsatisfiable/ ;
+        rm $newName ;
+        make clean ;
+    ) done ;
+) done ;
+
