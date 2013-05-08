93a839a2c30b40e88c8ca764583db53ef4f99cd8
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2013-05-08 13:39:17 +0200

    sudokuTransforme débuggé, devrait marcher maintenant

--- src/extensions/sudokuTransform.c
+++ src/extensions/sudokuTransform.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     unsigned short lin, col, nb, nb2;
     char c;
-    int grid[9][9];
+    short grid[9][9];
     if (argc != 2)
     {
         fprintf(stderr, "Nombre d'arguments invalide\n");
@@ -27,28 +27,33 @@ int main(int argc, char *argv[])
 
     
     /* On lit la grille d'entrée */
-    for (lin = 0; lin < 10; lin++)
+    for (lin = 0; lin < 9; lin++)
     {
-        for (col = 0; col < 10; col++)
+        for (col = 0; col < 9; col++)
         {
             c = fgetc(input);
+            putchar(c);
             if (c != '.')
                 grid[lin][col] = c - '0';
             else
                 grid[lin][col] = -1;
-            fgetc(input);
+            c=fgetc(input);
+            putchar(c);
         }
+        
     }
 
 
+    fclose(input);
+
     /* On fixe les contraintes dues à la grille lue */
-    for (lin = 0; lin < 10; lin++)
+    for (lin = 0; lin < 9; lin++)
     {
-        for (col = 0; col < 10; col++)
+        for (col = 0; col < 9; col++)
         {
             if (grid[lin][col] != -1)
             {
-                for (nb = 0; nb < 10; nb++)
+                for (nb = 1; nb <= 9; nb++)
                 {
                     if (nb != grid[lin][col])
                         putchar('~');
@@ -59,25 +64,18 @@ int main(int argc, char *argv[])
     }
 
 
-    fclose(input);
-    if (input != NULL)
-    {
-        fprintf(stderr, "Erreur lors de la fermeture du fichier d'entrée\n");
-        exit(3);
-    }
-
 
     /* On s'occupe de vérifier que chaque ligne a tous les nombres */
-    for (lin = 0; lin < 10; lin++)
+    for (lin = 0; lin < 9; lin++)
     {
-        for (nb = 0; nb < 10; nb++)
+        for (nb = 1; nb <= 9; nb++)
         {
             putchar('(');
-            for (col = 0; col < 9; col++)
+            for (col = 0; col < 8; col++)
             {
                 printf("s_%d_%d_%d \\/", lin, col, nb);
             }
-            printf("s_%d_%d_%d) /\\\n", lin, 9, nb);
+            printf("s_%d_%d_%d) /\\\n", lin, 8, nb);
         }
     }
 
@@ -85,16 +83,16 @@ int main(int argc, char *argv[])
 
 
     /* Maintenant on s'occupe de vérifier que chaque colonne a tous les nombres */
-    for (col = 0; col < 10; col++)
+    for (col = 0; col < 9; col++)
     {
-        for (nb = 0; nb < 10; nb++)
+        for (nb = 0; nb < 9; nb++)
         {
             putchar('(');
-            for (lin = 0; lin < 9; lin++)
+            for (lin = 0; lin < 8; lin++)
             {
                 printf("s_%d_%d_%d \\/", lin, col, nb);
             }
-            printf("s_%d_%d_%d) /\\\n", 9, col, nb);
+            printf("s_%d_%d_%d) /\\\n", 8, col, nb);
         }
     }
 
@@ -107,7 +105,7 @@ int main(int argc, char *argv[])
     {
         for (colBloc = 0; colBloc < 3; colBloc++)
         {
-            for (nb = 0; nb < 10; nb++)
+            for (nb = 1; nb <= 9; nb++)
             {
                 putchar('(');
                 for (lin = 3*linBloc; lin < 3*linBloc; lin++)
@@ -133,16 +131,16 @@ int main(int argc, char *argv[])
 
 
     /* Maintenant on vérifie qu'une case a un seul nombre */
-    for (lin = 0; lin < 10; lin++)
+    for (lin = 0; lin < 9; lin++)
     {
-        for (col = 0; col < 10; col++)
+        for (col = 0; col < 9; col++)
         {
-            for (nb = 0; nb < 9; nb++)
+            for (nb = 1; nb < 9; nb++)
             {
-                for (nb2 = nb+1; nb2 < 10; nb2++)
+                for (nb2 = nb+1; nb2 <= 9; nb2++)
                 {
                     printf("(~s_%d_%d_%d \\/ ~s_%d_%d_%d)", lin, col, nb, lin, col, nb2);
-                    if (!(lin == 9 && col == 9 && nb == 8))
+                    if (!(lin == 8 && col == 8 && nb == 8 && nb2 == 9))
                         printf("/\\");
                     putchar('\n');
                 }
