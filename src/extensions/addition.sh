f399217654bbbfc3b7e6cd8509c2e4e3a8c2a787
Julien Le Maire <julienlm38@gmail.com>
2013-05-21 01:00:42 +0200

    suppression de fichiers inutiles

--- src/extensions/addition.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./additionTransform $1 $2 | Tseitin/./tseitin -r foo.out > result.txt &&
-./additionPrint < result.txt
-#rm foo.out
-#rm result.txt
-
--- src/extensions/latinSquare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-k=$1
-./latinSquareTransform $k | Tseitin/./tseitin -r foo.out > result.txt
-./latinSquarePrint $k < result.txt
-
-#rm foo.out
-rm result.txt
-
